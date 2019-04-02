45dfd56b2f9cc9028d6cdae03a3d1b404efa160a
shishalive <49173437+shishalive@users.noreply.github.com>
2019-04-02 22:32:57 +0200

    Update module.php

--- Alarmanlage/module.php
+++ Alarmanlage/module.php
@@ -515,7 +515,7 @@
 			}
 		}
 			
-		private function setDeviceStatus(int $outputID, bool $Value, str $al_typ){
+		private function setDeviceStatus(int $outputID, bool $Value, string $al_typ){
 			if ($Value) {
 				$al_value = "true";
 			}else{
